51fb10cd0440275b35466830c74167245341094e
C <remedialchaostheory@users.noreply.github.com>
2019-05-31 17:20:18 -0700

    Refactor sorting and writing deposit data

--- squaredepositid.py
+++ squaredepositid.py
@@ -15,6 +15,7 @@ def format_date(orig_date):
     new_date = [fm_year, new_date_split[0], new_date_split[1]]
     return '-'.join(new_date)
 
+
 def calc_card_collected(fees, deposited):
     """
     Card collected = deposited - fees
@@ -26,7 +27,20 @@ def calc_card_collected(fees, deposited):
     """
     return round(deposited - fees, 2)
 
+
+def is_sorted(input_list):
+    """
+    Checks if sorted by sublist[0]
+    :param input_list: e.g. [['3', 'hello'], ['2', 'hi'],
+                            ['1', 'hey']]
+    :return: False
+    """
+    return all(input_list[i][0] <= input_list[i+1][0] for i in range(len(input_list)-1))
+
+
 def main():
+    # Uncomment for production
+    # ------------------------
     # Open a window to select the file (.csv)
     root = Tk()
     root.filename = filedialog.askopenfilename(
@@ -39,14 +53,31 @@ def main():
     print(base_path, 'base_path')
     original_fn = base_fn
 
-    # ---------------------
-
     # Open the csv file and create an array of each row
     raw_data = []
     with open(root.filename, 'r') as f:
         reader = csv.reader(f)
         for row in reader:
             raw_data.append(row)
+    # ------------------------
+
+    # Uncomment for testing
+    # ------------------
+    # test_file = ""
+    # print(test_file)
+    # base_fn = test_file.split('/')[-1]
+    # base_path = '/'.join(test_file.split('/')[:-1])
+    # print(base_fn)
+    # print(base_path, 'base_path')
+    # original_fn = base_fn
+
+    # Open the csv file and create an array of each row
+    # raw_data = []
+    # with open(test_file, 'r') as f:
+    #     reader = csv.reader(f)
+    #     for row in reader:
+    #         raw_data.append(row)
+    # ------------------
 
     # Get first and last date of the csv contents
     first_date = raw_data[-1][0].split(',')[0]
@@ -61,12 +92,6 @@ def main():
     # Format new filename
     new_fn = (base_path + '/' + 'square-qb-deposits-' +
               first_date + '-' + last_date + '.csv')
-
-    # deposit_date = [0]
-    # total_collected = [5]
-    # fees = [6]
-    # deposited = [7]
-    # deposit_id = [8]
 
     # Iterate through raw_data and save
     square = {}
@@ -103,48 +128,40 @@ def main():
             square[deposit_id][2] += fees
             square[deposit_id][3] += deposited
 
-
-        # square[deposit_id]['total_collected'] = square[deposit_id]['total_collected'] + total_collected
-
     # Round decimal places to 2
-    for id in square:
-        square[id][1] = round(square[id][1], 2)
-        square[id][2] = round(square[id][2], 2)
-        square[id][3] = round(square[id][3], 2)
+    for deposit_id in square:
+        square[deposit_id][1] = round(square[deposit_id][1], 2)
+        square[deposit_id][2] = round(square[deposit_id][2], 2)
+        square[deposit_id][3] = round(square[deposit_id][3], 2)
 
-    # Insert "card collected" values after "total_collected"
-    print("")
-    for id in square:
-        fees = square[id][2]
-        deposited = square[id][3]
+    # Square deposit detail files come in reverse chronological order
+    square_keys = list(square)
+    square_keys.reverse()
+
+    # Replace dictionary with list for sorting
+    deposit_list = []
+    for deposit_id in square_keys:
+        date = square[deposit_id][0]
+        total_collected = square[deposit_id][1]
+        fees = square[deposit_id][2]
+        deposited = square[deposit_id][3]
         card_collected = calc_card_collected(fees, deposited)
-        square[id].insert(2, card_collected)
+        deposit_list.append(
+            [date, deposit_id, total_collected, card_collected, fees, deposited])
+        del square[deposit_id]
 
-    print("square ", square)
+    if not is_sorted(deposit_list):
+        deposit_list.sort(key=operator.itemgetter(0))
 
-    # Write data to new .csv file
-    with open(new_fn, mode='w') as square_deposits:
-        square_writer = csv.writer(square_deposits, delimiter=',', quotechar='"')
+    print("deposit list ", deposit_list)
 
-        for id in square:
-            square_writer.writerow(
-                [square[id][0], id, square[id][1], square[id][2], square[id][3], square[id][4]])
-
-    square_deposits.close()
-
-    # TODO - can refactor this to only open file once by sorting beforehand
-    # Reopen file to sort by date
-    reader = csv.reader(open(new_fn), delimiter=";")
-    sorted_list = sorted(reader, key=operator.itemgetter(0), reverse=False)
-
-    # Write sorted data with headings
+    # Write deposit list to .csv with headings
     with open(new_fn, mode='w') as square_deposits:
         square_writer = csv.writer(square_deposits, delimiter=',', quotechar='"')
         square_writer.writerow(
             ['Deposit Date', 'Deposit ID', 'Total Collected', 'Card Collected', 'Fees', 'Deposited'])
 
-        for r in sorted_list:
-            row = r[0].split(',')
+        for row in deposit_list:
             print(row)
             square_writer.writerow([row[0], row[1], row[2], row[3], row[4], row[5]])
 
